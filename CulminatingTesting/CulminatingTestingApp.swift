b6c82ee4df7c0b0f314664f9d2f394b0b52dc9de
Judy Yu <judy.yuqiuyi@gmail.com>
2023-05-29 23:52:51 -0400

    Initial Commit

---
+++ CulminatingTesting/CulminatingTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingTestingApp.swift
+//  CulminatingTesting
+//
+//  Created by Judy YU on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
