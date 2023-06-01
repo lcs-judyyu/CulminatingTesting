fe6acfc5bbcd98af5cd29018c9396efd325f7083
Judy Yu <judy.yuqiuyi@gmail.com>
2023-05-31 21:40:48 -0400

    Add comments

--- CulminatingTesting/ContentView.swift
+++ CulminatingTesting/ContentView.swift
@@ -11,25 +11,31 @@ struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
             
-            VStack(spacing: 20) {
+            VStack(spacing: 15) {
                 
+                // Day & Location
                 HStack {
                     
                     Text("Day 1")
+                    
                     Spacer()
+                    
                     Text("The Coast")
                     
                 }
                 .font(.title3)
+                .padding(.horizontal)
                 
-                Image("Coast1")
+                // Illustration
+                Image("Coast3")
                     .resizable()
                     .scaledToFit()
                 
-                Text("There are two possible destinations where you can find resources to help you survive, where should you go? \n\nNote: it is an important decision which will influence the story development.")
+                // Description
+                Text("ENDING: You go inside the room and find a flashlight in the dark. You turn it on and find numerous rats in the hole in the corner. They all run out and attack you after being stimulated by the light source. Your weapon cannot fight against them. You can only give up all your food to escape. Any food left in the factory is contaminated. You die of starvation.")
                     .padding(.horizontal, 10)
                 
-                //Spacer()
+                Spacer()
                 
                 HStack {
                     
@@ -37,6 +43,7 @@ struct ContentView: View {
                     
                     VStack (alignment: .center, spacing: 20) {
                         
+                        // Choice 1
                         Button(action: {
                             
                         }, label: {
@@ -49,12 +56,14 @@ struct ContentView: View {
                         })
                         .buttonStyle(.borderedProminent)
                         
+                        // Choice 2
                         Button(action: {
                             
                         }, label: {
                             Spacer()
                             
                             Text("The Hospital")
+                                .foregroundColor(.white)
                             
                             Spacer()
                         })
@@ -71,14 +80,17 @@ struct ContentView: View {
             .padding()
             
             Spacer()
-            
+
+            // Display changes in values
             Text("Energy - 1, Food + 3")
-                .padding()
-            
+                .padding(.horizontal)
+
+            // Divider
             Divider()
                 .frame(height: 1.5)
                 .overlay(.white)
             
+            // Display values
             HStack(alignment: .top, spacing: 15) {
                 
                 Text("ME")
@@ -88,13 +100,16 @@ struct ContentView: View {
                     
                     HStack {
                         
+                        // Energy
                         Text("Energy")
                         ZStack(alignment: .center) {
+                            
+                            // Completion meter
                             Rectangle()
                                 .frame(width: 200, height: 22)
                                 .foregroundColor(.gray)
                             
-                            //borders
+                            // Borders
                             Rectangle()
                                 .stroke(.black, lineWidth: 2)
                                 .frame(width: 200, height: 22)
@@ -107,22 +122,29 @@ struct ContentView: View {
                         }
                         
                     }
+                    
+                    // Mentality & Food
                     HStack {
                         
                         Text("Mentality: Healthy")
                             .padding(.trailing, 20)
+                        
                         Image(systemName: "globe")
+                        
                         Text(": 4")
                         
                     }
                 }
                 
+                // Settings
                 Image(systemName: "gear")
                     .scaleEffect(2)
                     .padding(.leading, 10)
                     .padding(.top, 10)
+                
             }
-            .padding()
+            .padding(.horizontal)
+            .padding(.vertical, 5)
             
         }
         .padding(.bottom, 10)
@@ -130,6 +152,7 @@ struct ContentView: View {
     }
 }
 
+// Preview provider
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
