d05ef25ce2918328c9b4a0bea948848f179ab42a
Judy Yu <judy.yuqiuyi@gmail.com>
2023-06-07 00:54:21 -0400

    Animate blue size, blur opacity, text color, and text size

--- CulminatingTesting/AnimatedTextView.swift
+++ CulminatingTesting/AnimatedTextView.swift
@@ -9,27 +9,42 @@ import SwiftUI
 
 struct AnimatedTextView: View {
     
-    @State var color = Color.white
+    @State var color = Color.white.opacity(0.8)
     
     @State var size = 100.0
     
+    @State var blurOpacity = 0.6
+    
+    @State var blurSize = 70.0
+    
     var body: some View {
-        VStack {
-            Text("RAD")
-                .italic()
-            Text("ZONE")
+        ZStack {
+            
+            Ellipse()
+                .foregroundColor(.white.opacity(blurOpacity))
+                .frame(width: 300, height: 200)
+                .blur(radius: blurSize)
+            
+            VStack {
+                Text("RAD")
+                    .italic()
+                Text("ZONE")
+            }
+            .font(.custom("Copperplate", fixedSize: size))
+            .bold()
+            .foregroundColor(color)
+            .shadow(radius: 10)
+
         }
-        .font(.custom("Copperplate", fixedSize: size))
-        .bold()
-        .foregroundColor(color)
         .onAppear {
-            withAnimation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true)) {
-                size = 100.5
-            }
-            withAnimation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true)) {
-                color = .gray
+            withAnimation(.easeInOut(duration: 2).repeatForever(autoreverses: true)) {
+                size = 100.1
+                color = .white.opacity(0.5)
+                blurOpacity = 0.5
+                blurSize = 67.0
             }
         }
+
     }
 }
 
